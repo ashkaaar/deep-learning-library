00d4825144b12c3dc99a2cc6400f545e678ee20c
av1shkar <vishkardandge@gmail.com>
2023-12-17 21:47:33 +0530

    first commit

---
+++ lib/train.py
@@ -0,0 +1,27 @@
+"""
+Here's a function that can train a neural net
+"""
+
+from lib.tensor import Tensor
+from lib.nn import NeuralNet
+from lib.loss import Loss, MSE
+from lib.optim import Optimizer, SGD
+from lib.data import DataIterator, BatchIterator
+
+
+def train(net: NeuralNet,
+          inputs: Tensor,
+          targets: Tensor,
+          num_epochs: int = 5000,
+          iterator: DataIterator = BatchIterator(),
+          loss: Loss = MSE(),
+          optimizer: Optimizer = SGD()) -> None:
+    for epoch in range(num_epochs):
+        epoch_loss = 0.0
+        for batch in iterator(inputs, targets):
+            predicted = net.forward(batch.inputs)
+            epoch_loss += loss.loss(predicted, batch.targets)
+            grad = loss.grad(predicted, batch.targets)
+            net.backward(grad)
+            optimizer.step(net)
+        print(epoch, epoch_loss)
